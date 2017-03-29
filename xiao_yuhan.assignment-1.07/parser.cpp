30ee91acc27e5c8df3cd1be57d092d2dc0c2b3db
Struggler <xyhstruggler@gmail.com>
2017-03-29 14:54:28 -0500

    keepgoing

--- xiao_yuhan.assignment-1.07/parser.cpp
+++ xiao_yuhan.assignment-1.07/parser.cpp
@@ -52,13 +52,13 @@ int parse(vector<Monster_Definitions> &m)
 
       if(op == "NAME"){
         if(have_name){
-          cerr << "Duplicate field Name";
+          cerr << "Duplicate field Name\n" << endl;
           check = 1;
         }
         else{
           while (s.peek() == ' ') s.get();
           if(s.peek() == '\n'){
-            cerr << "Empty Name";
+            cerr << "Empty Name\n" << endl;
             check = 1;
           }
           getline(s, name);
@@ -68,40 +68,41 @@ int parse(vector<Monster_Definitions> &m)
 
       else if(op == "SYMB"){
         if(have_symb){
-          cerr << "Duplicate field Symbol";
+          cerr << "Duplicate field Symbol\n" << endl;
           check = 1;
         }
         else if(strlen(line.substr(5)) > 1){
-          cerr << "Incorrect length of Symbol";
+          cerr << "Incorrect length of Symbol\n" << endl;
           check = 1;
         }
         else{
           while (s.peek() == ' ') s.get();
           if(s.peek() == '/n'){
-            cerr << "Empty Symbol";
+            cerr << "Empty Symbol\n" << endl;
             check = 1;
           }
           getline(s, symbol);
-          have_name = 1;
+          have_symb = 1;
         }
       }
 
       else if(op == "DESC"){
         if(have_desc){
-          cerr << "Duplicate field Description";
+          cerr << "Duplicate field Description\n" << endl;
           check = 1;
         }
         else if(line.substr(5) != ""){
-          cerr << "Format wrong Description";
+          cerr << "Format wrong Description\n" << endl;
           check = 1;
         }
         else{
           have_desc = 1;
           while(!fs.eof()){
             getline(fs, line);
+            have_desc = 1;
             if(line.c_str()[0]=='.') break;
             else if(strlen(line) > 77){
-              cerr << "Length is too long for Description";
+              cerr << "Length is too long for Description\n" << endl;
               check = 1;
               break;
             }
@@ -115,54 +116,117 @@ int parse(vector<Monster_Definitions> &m)
 
       else if(op == "COLOR"){
         if(have_color){
-          cerr << "Duplicate field Color";
+          cerr << "Duplicate field Color\n" << endl;
           check = 1;
         }
         while (s.peek() == ' ') s.get();
         if(s.peek() == '/n'){
-          cerr << "Empty Color";
+          cerr << "Empty Color\n" << endl;
           check = 1;
         }
         getline(s, color);
-        if(color != "BLACK" || color != "RED" || color != "GREEN" || color != "YELLOW" || color != "BLUE" ||
-           color != "MAGENTA" || color != "CYAN" || color != "WHITE" || ){
-             cerr << "Wrong Color";
+        have_color = 1;
+        if(!(color == "BLACK" || color == "RED" || color == "GREEN" || color == "YELLOW" || color == "BLUE" ||
+           color == "MAGENTA" || color == "CYAN" || color == "WHITE") ){
+             cerr << "Wrong Color\n" << endl;
              check = 1;
            }
       }
 
       else if(op == "SPEED"){
+        int base, dices, sides;
         if(have_speed){
-          cerr << "Duplicate field Speed";
+          cerr << "Duplicate field Speed\n" << endl;
           check = 1;
         }
         while (s.peek() == ' ') s.get();
         if(s.peek() == '/n'){
-          cerr << "Empty Speed";
+          cerr << "Empty Speed\n" << endl;
           check = 1;
         }
         getline(s, speed);
-
-
+        have_speed = 1;
+        if(sscanf(speed.c_str(), "%d+%dd%d", &base, &dices, &sides) != 3){
+          check = 1;
+        }
       }
 
       else if(op == "DAM"){
-
+        int base, dices, sides;
+        if(have_dam){
+          cerr << "Duplicate field Damage\n" << endl;
+          check = 1;
+        }
+        while (s.peek() == ' ') s.get();
+        if(s.peek() == '/n'){
+          cerr << "Empty Damage\n" << endl;
+          check = 1;
+        }
+        getline(s, dam);
+        have_dam = 1;
+        if(sscanf(dam.c_str(), "%d+%dd%d", &base, &dices, &sides) != 3){
+          check = 1;
+        }
       }
 
       else if(op == "HP"){
-
+        int base, dices, sides;
+        if(have_hp){
+          cerr << "Duplicate field HP\n" << endl;
+          check = 1;
+        }
+        while (s.peek() == ' ') s.get();
+        if(s.peek() == '/n'){
+          cerr << "Empty HP";
+          check = 1;
+        }
+        getline(s, hp);
+        have_hp = 1;
+        if(sscanf(hp.c_str(), "%d+%dd%d", &base, &dices, &sides) != 3){
+          check = 1;
+        }
       }
 
       else if(op == "ABIL"){
-
+        if(have_abil){
+          cerr << "Duplicate field Ability\n" << endl;
+          check = 1;
+        }
+        while (s.peek() == ' ') s.get();
+        if(s.peek() == '/n'){
+          cerr << "Empty ABIL\n" << endl;
+          check = 1;
+        }
+        getline(s, abil);
+        have_abil = 1;
+        if(!(abil == "SMART" || abil == "TELE" || abil == "TUNNEL" || abil == "ERRATIC" || abil == "PASS," ||
+           abil == "PICKUP" || abil == "DESTROY")){
+             check = 1;
+           }
       }
-
       else if(op == "END"){
-        break;
+        cerr << "Wrong number of descriptions\n" << endl;
+        check = 1;
       }
-      else check = 1;
+      else {
+        cerr << "Wrong name of descriptions\n" << endl;
+        check = 1;
+      }
     }
+    if(!getline(fs, line)){
+      cerr << "No END";
+      check = 1;
+    }
+    getline(fs, line);
+    if(line != "END"){
+      cerr << "Wrong number of descriptions\n" << endl;
+      check = 1;
+    }
+    if(!check){
+      
+    }
+
+
   }
 
 

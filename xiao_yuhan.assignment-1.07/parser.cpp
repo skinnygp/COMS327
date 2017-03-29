1078b681831594a720aa8f062c8fc83b9b9cf1f3
Struggler <xyhstruggler@gmail.com>
2017-03-29 13:15:37 -0500

    working

--- xiao_yuhan.assignment-1.07/parser.cpp
+++ xiao_yuhan.assignment-1.07/parser.cpp
@@ -40,48 +40,128 @@ int parse(vector<Monster_Definitions> &m)
       getline(fs, line);
     }
     int count;
-    for(getline(file,line), count = 0; count < 8; count ++){
+    int check = 0;
+    for(!fs.eof(), count = 0, !check; count < 8; count ++){
       int have_name, have_desc, have_color, have_speed, have_symb, have_dam, have_hp, have_abil;
       Monster_Definitions m;
       string name, desc, color, speed, symb, dam, hp, abil;
-      getline(file, line);
+      getline(fs, line);
+      string op;
+			stringstream s(line);
+			s >> op;
 
-      if(line.substr(0, 4) == "NAME"){
+      if(op == "NAME"){
         if(have_name){
           cerr << "Duplicate field Name";
-        }
-        else if(line.substr(5) == ""){
-          cerr << "Empty Name";
+          check = 1;
         }
         else{
-          name = line.substr(5);
+          while (s.peek() == ' ') s.get();
+          if(s.peek() == '\n'){
+            cerr << "Empty Name";
+            check = 1;
+          }
+          getline(s, name);
           have_name = 1;
         }
       }
 
-      else if(line.substr(0, 4) == "SYMB"){
+      else if(op == "SYMB"){
         if(have_symb){
           cerr << "Duplicate field Symbol";
-        }
-        else if(line.substr(5) == ""){
-          cerr << "Empty Symbol";
+          check = 1;
         }
         else if(strlen(line.substr(5)) > 1){
           cerr << "Incorrect length of Symbol";
+          check = 1;
         }
         else{
-          symbol = line.substr(5);
+          while (s.peek() == ' ') s.get();
+          if(s.peek() == '/n'){
+            cerr << "Empty Symbol";
+            check = 1;
+          }
+          getline(s, symbol);
           have_name = 1;
         }
       }
 
-      else if(line.substr(0, 4) == "DESC"){
+      else if(op == "DESC"){
         if(have_desc){
           cerr << "Duplicate field Description";
+          check = 1;
         }
-        else if()
+        else if(line.substr(5) != ""){
+          cerr << "Format wrong Description";
+          check = 1;
+        }
+        else{
+          have_desc = 1;
+          while(!fs.eof()){
+            getline(fs, line);
+            if(line.c_str()[0]=='.') break;
+            else if(strlen(line) > 77){
+              cerr << "Length is too long for Description";
+              check = 1;
+              break;
+            }
+            else {
+              desc += line;
+              desc += "\n";
+            }
+          }
+        }
       }
 
+      else if(op == "COLOR"){
+        if(have_color){
+          cerr << "Duplicate field Color";
+          check = 1;
+        }
+        while (s.peek() == ' ') s.get();
+        if(s.peek() == '/n'){
+          cerr << "Empty Color";
+          check = 1;
+        }
+        getline(s, color);
+        if(color != "BLACK" || color != "RED" || color != "GREEN" || color != "YELLOW" || color != "BLUE" ||
+           color != "MAGENTA" || color != "CYAN" || color != "WHITE" || ){
+             cerr << "Wrong Color";
+             check = 1;
+           }
+      }
+
+      else if(op == "SPEED"){
+        if(have_speed){
+          cerr << "Duplicate field Speed";
+          check = 1;
+        }
+        while (s.peek() == ' ') s.get();
+        if(s.peek() == '/n'){
+          cerr << "Empty Speed";
+          check = 1;
+        }
+        getline(s, speed);
+
+
+      }
+
+      else if(op == "DAM"){
+
+      }
+
+      else if(op == "HP"){
+
+      }
+
+      else if(op == "ABIL"){
+
+      }
+
+      else if(op == "END"){
+        break;
+      }
+      else check = 1;
     }
   }
 

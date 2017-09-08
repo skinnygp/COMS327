c4b0cf12c91c445730751332b818df6de77e4419
Struggler <xyhstruggler@gmail.com>
2017-09-07 23:50:31 -0500

    test

--- Notes/week1_s2017/hello.c
+++ Notes/week1_s2017/hello.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[])
+#include <math.h>
+int main()
 {
-  printf("Hello World!\n");
-
-  return 0;
+double w=1234.789;
+double x=0;
+double y;
+double *z;
+ z=(double *)&w;
+ scanf("%f", &y);
+ *z=sqrt(w);
+ y=y*2.0;
+ printf("y=%f, *z=%f\n", y, *z);
+ y=y/x;
+ w=y*z;
+ return 0;
 }
